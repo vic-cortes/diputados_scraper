45e83d3a2ba19ab8bedb5900668d364b4a5b6166
Víctor M. Cortés Figueroa <vcortes@datuminference.com>
2023-01-20 16:59:42 -0600

    Se separa clase de archivo main

--- src/main.py
+++ src/main.py
@@ -1,25 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
+from tools.deputy.header_info import DeputyHeaderInfo
 
-MONTH_NAMES = {
-    "ENERO": "01",
-    "FEBRERO": "02",
-    "MARZO": "03",
-    "ABRIL": "04",
-    "MAYO": "05",
-    "JUNIO": "06",
-    "JULIO": "07",
-    "AGOSTO": "08",
-    "SEPTIEMBRE": "09",
-    "OCTUBRE": "10",
-    "NOVIEMBRE": "11",
-    "DICIEMBRE": "12",
-}
+
 BASE_URL = "http://sitl.diputados.gob.mx/LXIV_leg/curricula.php"
 
 
-response = requests.get(url=BASE_URL, params={"dipt": 1})
+response = requests.get(url=BASE_URL, params={"dipt": 41})
 
 html = response.content
 soup = BeautifulSoup(html, "lxml")
@@ -28,67 +16,10 @@ tables = soup.find_all("table")
 _, information, *_ = tables
 
 html_table = information.find("table")
-candidate_table = html_table.find("table")
-candidate_name, *rows = [row.text.strip() for row in candidate_table.find_all("tr")]
-
-election_type, entity, email, date_birth, alternate = rows
+deputy_table = html_table.find("table")
 
 
-class DeputyHeaderInfo:
-    def clean_row_election_type(raw_election_type: str) -> dict:
-        """
-        Limpia y obtinen la informacion de manera organizada
-        del tipo de eleccion del diputado
-        """
-        _, election_type = raw_election_type.split(":")
+if __name__ == "__main__":
+    deputy = DeputyHeaderInfo(deputy_table=deputy_table)
 
-        return {"tipo_eleccion": election_type.strip()}
-
-    def clean_row_alternate(raw_alternate: str) -> dict:
-        """
-        Limpia y obtinen la informacion de manera organizada
-        del tipo de eleccion del diputado
-        """
-        _, alternate = raw_alternate.split(":")
-
-        return {"suplente": alternate.strip()}
-
-    def clean_row_birth_day(raw_day_birth: str) -> dict:
-        """
-        Limpia y obtinen la informacion de manera organizada
-        la fecha de nacimiento
-        """
-        _, date_birth = raw_day_birth.split(":")
-        day, month_name, year = date_birth.upper().split("-")
-        day = day.strip().zfill(2)
-        month = MONTH_NAMES.get(month_name)
-        final_date = f"{year}-{month}-{day}"
-
-        return {"fecha_nacimiento": final_date}
-
-    def clean_row_entity(raw_entity: str) -> dict:
-        """
-        Limpia y obtinen la informacion de manera organizada
-        de la entdidad del diputado
-        """
-        entity_name, district, district_code = [
-            el.strip() for el in raw_entity.split("|")
-        ]
-
-        _, state = entity_name.split(":")
-        _, district = district.split(":")
-
-        return {
-            "entidad": state.strip(),
-            "distrito": district.strip(),
-            "codig_distrito": district_code.strip(),
-        }
-
-    def clean_row_email(raw_email: str) -> dict:
-        """
-        Limpia y obtinen la informacion de manera organizada
-        del renglon de email
-        """
-        _, _, email, _, extension = raw_email.split()
-
-        return {"email": email.strip(), "extension": extension.strip()}
+    print(deputy.get_info())
---
+++ src/tools/deputy/header_info.py
@@ -0,0 +1,111 @@
+from dataclasses import dataclass
+
+from bs4.element import Tag
+
+
+MONTH_NAMES = {
+    "ENERO": "01",
+    "FEBRERO": "02",
+    "MARZO": "03",
+    "ABRIL": "04",
+    "MAYO": "05",
+    "JUNIO": "06",
+    "JULIO": "07",
+    "AGOSTO": "08",
+    "SEPTIEMBRE": "09",
+    "OCTUBRE": "10",
+    "NOVIEMBRE": "11",
+    "DICIEMBRE": "12",
+}
+
+
+@dataclass
+class DeputyHeaderInfo:
+    """
+    Clase encargada de extraer informacion de diputado de pagina principal
+    """
+
+    deputy_table: Tag
+
+    @staticmethod
+    def clean_row_election_type(raw_election_type: str) -> dict:
+        """
+        Limpia y obtinen la informacion de manera organizada
+        del tipo de eleccion del diputado
+        """
+        _, election_type = raw_election_type.split(":")
+
+        return {"tipo_eleccion": election_type.strip()}
+
+    @staticmethod
+    def clean_row_alternate(raw_alternate: str) -> dict:
+        """
+        Limpia y obtinen la informacion de manera organizada
+        del tipo de eleccion del diputado
+        """
+        _, alternate = raw_alternate.split(":")
+
+        return {"suplente": alternate.strip()}
+
+    @staticmethod
+    def clean_row_date_birth(raw_day_birth: str) -> dict:
+        """
+        Limpia y obtinen la informacion de manera organizada
+        la fecha de nacimiento
+        """
+        _, date_birth = raw_day_birth.split(":")
+        day, month_name, year = date_birth.upper().split("-")
+        day = day.strip().zfill(2)
+        month = MONTH_NAMES.get(month_name)
+        final_date = f"{year}-{month}-{day}"
+
+        return {"fecha_nacimiento": final_date}
+
+    @staticmethod
+    def clean_row_entity(raw_entity: str) -> dict:
+        """
+        Limpia y obtinen la informacion de manera organizada
+        de la entdidad del diputado
+        """
+        entity_name, district, district_code = [
+            el.strip() for el in raw_entity.split("|")
+        ]
+
+        _, state = entity_name.split(":")
+        _, district = district.split(":")
+
+        return {
+            "entidad": state.strip(),
+            "distrito": district.strip(),
+            "codig_distrito": district_code.strip(),
+        }
+
+    @staticmethod
+    def clean_row_email(raw_email: str) -> dict:
+        """
+        Limpia y obtinen la informacion de manera organizada
+        del renglon de email
+        """
+        _, _, email, _, extension = raw_email.split()
+
+        return {"email": email.strip(), "extension": extension.strip()}
+
+    def get_info(self):
+        deputy_name, *rows = [
+            row.text.strip() for row in self.deputy_table.find_all("tr")
+        ]
+
+        election_type, entity, email, date_birth, alternate = rows
+
+        deputy_info = {"diputado": deputy_name}
+        deputy_info |= self.clean_row_election_type(election_type)
+        deputy_info |= self.clean_row_entity(entity)
+        deputy_info |= self.clean_row_email(email)
+        deputy_info |= self.clean_row_date_birth(date_birth)
+        deputy_info |= self.clean_row_alternate(alternate)
+
+        return deputy_info
+
+
+if __name__ == "__main__":
+    pass
