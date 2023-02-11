40419ec133938985975be71fa519ccc0978fe3d8
Víctor M. Cortés Figueroa <vmcortesf@gmail.com>
2023-02-11 00:25:17 -0600

    Se corrige test para extraccion de información de diputado

--- src/tests.py
+++ src/tests.py
@@ -13,13 +13,23 @@ class DeputyHeaderTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.html = open(f"{DATA_PATH}/test_deputy_main_page.html", "r").read()
         self.soup = BeautifulSoup(self.html, "lxml")
+        self.header_table = self.soup.find_all("table")[
+            1
+        ]  # Header table es el segundo elemento
+        self.header_table = self.header_table.find("table")
 
+    @unittest.skip("")
     def test_path_name(self) -> None:
         print_title("Test find tables")
-
         tables = self.soup.find_all("table")
-
         self.assertEqual(len(tables), 17)
+
+    # @unittest.skip("")
+    def test_header_deputy(self) -> None:
+        print_title("test header")
+        deputy_header = LXIVDeputyHeaderInfo(header_table=self.header_table)
+        print(deputy_header)
+        self.assertIsInstance(deputy_header.get_info(), dict)
 
 
 if __name__ == "__main__":
